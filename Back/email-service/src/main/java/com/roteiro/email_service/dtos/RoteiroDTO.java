4fb62b0e6299dca14ec0154e53a99391fb927660
Maria Aline Mees <mariaalinemees@gmail.com>
2025-11-21 09:16:15 -0300

    feat: Adiciona campo de e-mail no cadastro e envia e-mail para usuário pelo Mailtrap

---
+++ Back/email-service/src/main/java/com/roteiro/email_service/dtos/RoteiroDTO.java
@@ -0,0 +1,20 @@
+package com.roteiro.email_service.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoteiroDTO implements Serializable {
+    private Long id;
+    private String titulo;
+    private String destino;
+    private String dataInicio;
+    private String dataFim;
+    private double custoEstimado;
+    private String userEmail; // E-mail do usuário
+}
